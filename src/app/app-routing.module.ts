e847e71641d42102824a29ede8ecacc2a863ffcf
Pablo Jimenez <pablojavierjimenez@gmail.com>
2020-10-08 07:34:48 -0300

    seting up full routes to home, and unknowed routes to not found pages respectively

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', loadChildren: () => import('./page/home/home.module').then(m => m.HomeModule) },
-  { path: 'notFound', loadChildren: () => import('./page/not-found/not-found.module').then(m => m.NotFoundModule) }
+  { path: 'notFound', loadChildren: () => import('./page/not-found/not-found.module').then(m => m.NotFoundModule) },
+  {path: '404', loadChildren: () => import('./page/not-found/not-found.module').then(m => m.NotFoundModule)},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
